41948505d0be1a068cb7356109bff1fe9bf49ebb
DESKTOP-6KCQLT1\yogas <yogabagas69@gmail.com>
2021-06-29 10:28:07 +0700

    add push alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,16 @@
     });
   });
 </script>
+<!-- PushAlert -->
+<script type="text/javascript">
+    (function(d, t) {
+            var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+            g.src = "https://cdn.pushalert.co/integrate_a51e047badb97795042ef203da35ca64.js";
+            s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+</script>
+<!-- End PushAlert -->
 </head>
 <body>
     <div id="app">
